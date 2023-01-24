e72b0588671aaad6d35d12c14f15b72252d58bce
Md-Alqma <mdalqma7@gmail.com>
2023-01-25 01:13:35 +0530

    Registration successful with jwt token generated

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import apiRoute from "./utils/api.js";
+import { DB_CONNECT } from "./utils/constants.js";
 
 const app = express();
 
+mongoose.connect(DB_CONNECT, { useNewUrlParser: true }, (e) => console.log(e));
 const PORT = 8000;
 
+app.use(express.json());
 app.use("/api/", apiRoute);
 
-app.listen(PORT, () =>
-  console.log(`Server is listening on localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server is listening on localhost:${PORT}`));
---
+++ server/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const DB_CONNECT = `mongodb+srv://alqma:AlqmaAnsari@cluster0.iutp4ht.mongodb.net/todo?retryWrites=true&w=majority`;
+
+export const JWT_TOKEN_SECRET = "asdfkjsdgfjlds";
+
+export const StatusCode = {
+  SUCCESS: 200,
+  VALIDATION_ERROR: 201,
+  UNPROCESSABLE_ENTITY: 202,
+};
