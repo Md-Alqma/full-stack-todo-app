67542cc0528afc9d98512de169cd9a956d4eac4e
Md-Alqma <mdalqma7@gmail.com>
2023-01-25 18:00:37 +0530

    detetion form todolist is done

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -7,6 +7,7 @@ import { createTodo } from "../controllers/Todo.controller.js";
 import { check } from "express-validator";
 import { GetTodos } from "../controllers/TodoList.controller.js";
 import { MarkTodo } from "../controllers/MarkTodo.controller.js";
+import { RemoveTodo } from "../controllers/RemoveTodo.controller.js";
 const apiRoute = express.Router();
 export const apiProtected = express.Router();
 
@@ -25,6 +26,12 @@ apiProtected.post(
   "/marktodo",
   [check("todo_id", "Todo id is required").exists()],
   MarkTodo
+);
+
+apiProtected.post(
+  "/deletetodo",
+  [check("todo_id", "Todo id is required").exists()],
+  RemoveTodo
 );
 
 apiProtected.get("/todolist", GetTodos);
