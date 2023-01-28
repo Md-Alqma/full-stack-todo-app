b7a0e8f00f52a27c47a145d2cfee68c2ccec4dbd
Md-Alqma <mdalqma7@gmail.com>
2023-01-28 16:36:01 +0530

    register form UI completed

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-md-center mt-4">
+          <div className="col-lg-5 card border-primary mb-3">
+            <div className="card-header h4 text-center">
+              Register an account
+            </div>
+            <div className="card-body">
+              <div className="form-group">
+                <label className="col-form-label mt-4">Username</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Username"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label mt-4">Email</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Your Email"
+                />
+              </div>
+              <div className="form-group">
+                <label className="col-form-label mt-4">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter Your Password"
+                />
+              </div>
+
+              <div className="row justify-content-md-center  form-group mt-4" >
+                <button type="button" className="col-sm-6 btn btn-outline-secondary center">
+                  Register Now
+                </button>
+              </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Register;
