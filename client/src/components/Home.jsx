5246a9e9cc3b51685f21dc0931b00761d6dceb0b
Md-Alqma <mdalqma7@gmail.com>
2023-01-28 17:35:34 +0530

    created home UI and add todo UI

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import Todo from "./partials/Todo";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row justify-content-md-center mt-4">
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+          <Todo />
+        </div>
+      </div>
+      <div
+        className=""
+        style={{ position: "fixed", right: 50, bottom: 50, zIndex: 999 }}>
+        <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+          className="btn btn-outline-light">
+          Add Todo
+        </button>
+      </div>
+
+      <div className="modal mt-5" id="exampleModal">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <div className="modal-title">Add new Todo</div>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="close">
+                <span arial-hidden="true"></span>
+              </button>
+            </div>
+            <div className="modal-body">
+              <div className="form-group">
+                <textarea
+                  name=""
+                  className="form-control"
+                  rows={3}
+                  placeholder="Write todo..."></textarea>
+              </div>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-secondary" data-bs-dismiss="modal">
+                Close
+              </button>
+              <button className="btn btn-secondary">Add Todo</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
---
+++ client/src/components/partials/Todo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <div className="col-sm-3 mx-3 my-2 alert bg-light">
+      <div className="card-header">Header</div>
+      <div className="card-body">
+        <h4 className="card-title">Light card title</h4>
+        <p className="card-text">Some text example</p>
+          </div>
+          
+    </div>
+  );
+};
+
+export default Todo;
